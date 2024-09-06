3625d4fc2e9290c6d36353cfb9efa57a6ab13467
Ricardolopez31 <133120914+Ricardolopez31@users.noreply.github.com>
2024-09-06 18:52:32 +0000

    proposito y primer input

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+import <iostream>
 
+int main(){
+    
+    //desplegar proposito del programa
+    std::cout<<"Este programa calcula la cantidad de dinero que hizo un cine y el distribudor de la pelicula" << endl;
+
+    //establecer variables
+    int Num_Nin, Num_Adu, Num_Sen,;
+
+    //pedir input de taquillas vendidas
+    std::cout<< "Cantidad de taquillas de niños vendidas: "<< endl;
+    std::cin>>Num_Nin
+
+
+
+}
