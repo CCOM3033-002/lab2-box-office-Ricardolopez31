d65c9b365e7854d4ec31b7f5447f3a4d40979460
Ricardolopez31 <133120914+Ricardolopez31@users.noreply.github.com>
2024-09-06 19:23:45 +0000

    tratando de aplicar getline

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
-import <iostream>
-
+#include <iostream>
+#include <string>
 int main(){
     
     //desplegar proposito del programa
-    std::cout<<"Este programa calcula la cantidad de dinero que hizo un cine y el distribudor de la pelicula" << endl;
+    std::cout<<"Este programa calcula la cantidad de dinero que hizo un cine y el distribudor de la pelicula" << std::endl;
 
     //establecer variables
-    int Num_Nin, Num_Adu, Num_Sen,;
+   int Num_Nin, Num_Adu, movie ;
+ 
+    double Adulto_tik = 10.00;
+    double Niño_tik = 6.00;
+
+    // Pedir nombre de pelicula
+    std::cout << "Nombre de la pelicula vendida?: ";
+    std::cin>>movie;
+    getline(cin, movie);
 
     //pedir input de taquillas vendidas
-    std::cout<< "Cantidad de taquillas de niños vendidas: "<< endl;
-    std::cin>>Num_Nin
+    std::cout<< "Cantidad de taquillas de Niños vendidas?: ";
+    std::cin>>Num_Nin;
+
+    std::cout<< "Cantidad de taquillas de Adultos vendidas?: ";
+    std::cin>>Num_Adu;
 
 
+    //
 
+    return 0;
 }
